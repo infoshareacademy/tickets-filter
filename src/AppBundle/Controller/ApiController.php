98040cb86654fee4404b4adee25f55fcdcae6744
katban <grancen@wp.pl>
2015-11-12 17:12:43 +0100

    TIC-12: wiem co mam zrobić

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -6,18 +6,43 @@ use AppBundle\Utils\Ticket;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends Controller
 {
+    /**
+     * @Route("/dummyRestServer")
+     * description: aaaaa... póki poprzednia aplikacja nie jest skończona - generuje gloopie dane ;)
+     */
+    public function dummyRestServerAction()
+    {
+        $dane1 = new Ticket('Bilety Polska Czechy Bilet Wr...IO HIT', 'http://allegro.pl/show_item.php?item=57621555201','description1', 15, 'sport' );
+        $dane2 = new Ticket('Bilety inny ', 'http://allegro.pl/show_item.php?item=57621533201','description32', 5, 'sport' );
+        $dane3 = new Ticket('Bilety trzeci ', 'http://allegro.pl/show_item.php?item=67821533201','description long', 10, 'sport' );
+
+        $table = array($dane1, $dane2, $dane3);
+
+
+        return new JsonResponse($table);
+    }
+
+
+
+
     /**
      * @Route("/import")
      */
     public function importAction()
     {
-        $result = $this->callApi('GET', 'http://localhost:8080/api-browser/app_dev.php/dummyRestServer');
-
-
-        return '';
+        $result = $this->callApi('GET', 'http://localhost:8080/tickets-filter/app_dev.php/dummyRestServer');
+        $dateFromRest = json_decode($result);
+        print_r($dateFromRest);
+        // Piotrka clasa wyszukująca
+        //tworze instancje jego klasy
+        //wywoluje metody jego klasy na moich $dataFromRest
+        //zwracam nowego JsonResponsa (od wyfiltorwanych danych)
+        return new Response($dateFromRest);
     }
 
     private function callApi($method, $url, $data = false)
