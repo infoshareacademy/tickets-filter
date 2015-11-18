e1287ed61f43e1ee510c3c5a350f9ba0fda5613b
krasai <krasuskip@gmail.com>
2015-11-18 15:18:02 +0100

    [TIC 13] -  Filter Class with corrections for stripos ()

--- src/AppBundle/Tests/Utils/FilterTest.php
+++ src/AppBundle/Tests/Utils/FilterTest.php
@@ -10,8 +10,20 @@ namespace AppBundle\Tests\Utils;
 
 use AppBundle\Utils\Filter;
 
-class FilterTest extends PHPUnit_Framework_TestCase
+class FilterTest extends \PHPUnit_Framework_TestCase
 {
 
+    public function testFilterData()
+    {
+        // given
+        $connector = $this->getMock('Tickets\Connectors\AllegroConnector');
+        $connector->expects($this->any())->method('getItems')->will($this->returnValue(['error'=> 1]));
+        $instance = new AllegroBridge($connector);
 
+        // when
+        $result = $instance->getSportTickets();
+
+        // then
+        $this->assertEquals(['error'=> 1], $result);
+    }
 }
